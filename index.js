0937f87b2e6d2cc0439b0d9c38e2dcb19e449290
Pavan Shetty <pavanshettyhome@Pavans-MacBook-Air.local>
2021-07-18 01:01:53 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,81 @@
+const { json } = require('express');
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const generateScraperUrl = (api_key) => `http://api.scraperapi.com?api_key=${api_key}&autoparse=true`;
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Amazon Scrapper API");
+});
+
+// GET PRODUCTS
+app.get('/products/:productId', async (req, res) => {
+    const {productId} = req.params;
+    const {api_key} = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+    }
+
+});
+
+// GET REVIEWS
+app.get('/products/:productId/reviews', async (req, res) => {
+    const {productId} = req.params;
+    const {api_key} = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+    }
+
+});
+
+// GET PRODUCT OFFERS
+app.get('/products/:productId/offers', async (req, res) => {
+    const {productId} = req.params;
+    const {api_key} = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+    }
+
+});
+
+// GET SEARCH RESULTS
+app.get('/search/:searchQuery', async (req, res) => {
+    const {searchQuery} = req.params;
+    const {api_key} = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error);
+    }
+
+});
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
